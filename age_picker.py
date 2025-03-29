4afd383855ac9c99e223808947192ef13e859b0c
jaelyn <nievesjaelyn5@gmail.com>
2025-03-29 00:29:24 -0400

    Ap  Lab 5

---
+++ age_picker.py
@@ -0,0 +1,40 @@
+class AgePicker:
+    def __init__(self, toddler: int,adolescence: int,teen: int,adult:int,elderly: int):
+        self.age_center = {
+            'toddler': toddler,
+            'adolescence': adolescence,
+            'teen': teen,
+            'adult': adult,
+            'elderly': elderly} 
+
+    def add(self, age_center: str) -> None:
+        if age_center == 'toddler':
+            self.age_center['toddler'] = self.age_center.get('toddler') +1
+        if age_center == 'adolescence':
+            self.age_center['adolescence'] = self.age_center.get('adolescence') +1
+        if age_center == 'teen':
+            self.age_center['teen'] = self.age_center.get('teen') +1
+        if age_center == 'adult':
+            self.age_center['adult'] = self.age_center.get('adult') +1
+        if age_center == 'elderly':
+            self.age_center['elderly'] = self.age_center.get('elderly') +1
+
+
+    def sort(self) -> str:
+        score = 0
+        result = ''
+        for age, points in self.age_center.items():
+            if points > score:
+                score = points
+                result = age
+
+        return result
+    
+
+def clear(self) -> None:
+    self.age_center = {
+        'toddler': 0,
+        'adolescence': 0,
+        'teen': 0,
+        'adult': 0,
+        'elderly': 0,}
--- app.py
+++ app.py
@@ -1,13 +1,83 @@
-from flask import Flask 
-
+from flask import Flask, request, render_template, redirect
+from age_picker import AgePicker
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello world"
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run()
+
+age_picker = AgePicker(0, 0, 0, 0, 0)
+
+
+@app.route('/')
+def home():
+    return "Lets guess your age"
+
+@app.route('/question/1', methods = ['GET', 'POST'])
+def first_question():
+    answers = ['Youtube', 'Tiktok', 'Instagram', 'Myspace', 'Facebook']
+
+    if request.method == 'GET':
+        return render_template('question_1.html', answers = answers)
+    
+    if request.method == 'POST':
+        selected == request.form['selected'] 
+        if selected == answers[0]:
+            age_picker.add('toddler')
+        if selected == answers[1]:
+            age_picker.add('child')
+        if selected == answers[2]:
+            age_picker.add('teen')
+        if selected == answers[3]:
+            age_picker.add('adult')
+        if selected == answers[4]:
+            age_picker.add('elderly')
+        
+        return redirect('/question/2')
+    
+@app.route('/question/2', methods = ['GET', "POST"])
+def second_question():
+    answers = ['Ms.Rachel', 'Taylor Swift', 'Rihanna', 'New kids on the Block', 'Elvis Presley']
+
+    if request.method == 'GET':
+        return render_template('question_2.html', answers = answers)
+    
+    if request.method == 'POST':
+        selected == request.form['selected']
+        if selected == answers[0]:
+            age_picker.add('toddler')
+        if selected == answers[1]:
+            age_picker.add('child')
+        if selected == answers[2]:
+            age_picker.add('teen')
+        if selected == answers[3]:
+            age_picker.add('adult')
+        if selected == answers[4]:
+            age_picker.add('elderly')
+
+        return redirect('/question/3')
+    
+
+@app.route('/question/3', methods = ['GET', 'POST'])
+def third_question():
+    answers = ['Bluey', 'Pj mask', 'Gravity Falls', 'Full house', 'The Brady Brunch']
+
+    if request.method == 'GET':
+        return render_template('question_3.html', answers = answers)
+    
+    if request.method == 'POST':
+        selected == request.form.get['selected']
+        if selected == answers[0]:
+            age_picker.add('toddler')
+        if selected == answers[1]:
+            age_picker.add('child')
+        if selected == answers[2]:
+            age_picker.add('teen')
+        if selected == answers[3]:
+            age_picker.add('adult')
+        if selected == answers[4]:
+            age_picker.add('elderly')
+        return redirect('/age')
+    
+@app.route('/age')
+def get_age():
+    return 'YOU AREEEEEE' + age_picker.sort() + '!' + 'Congratulations or not...'
